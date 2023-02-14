8d443d8ee5ec304dae6cb0a67a49ac538470e06a
bunny22 <10805620+bunny_22@user.noreply.gitee.com>
2023-02-14 16:11:19 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
+<script>
+  export default{
+    name: 'Index',//组件名
+    components:{  //包含的子组件
+
+    },
+    data(){//双向绑定的数据
+      return{
+
+      }
+    },
+    methods:{//自定义方法
+
+    },
+    mounted(){//钩子函数-vue加载完时，执行的函数
+
+    },
+    created(){//vue对象创建时执行
+
+    },
+    beforeUpdate(){//组件更改之前执行的函数
+
+    },
+    updated(){//组件 更改时 执行的函数
+
+    },
+    destroyed() {//组件销毁时执行的函数
+      
+    },
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
